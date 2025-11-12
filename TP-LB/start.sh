d06824d125579705020dadb202590980597e59d6
root <rooot1@nethandeb.myguest.virtualbox.org>
2025-11-12 15:50:41 +0100

    Ajout des fichiers et du workflow de base

---
+++ TP-LB/start.sh
@@ -0,0 +1,23 @@
+docker network create tplb
+docker build -t im-nginx-lb ./tp-A
+mkdir -p shared1 shared2
+echo "<h1>Hello 1</h1>" > shared1/index.html
+echo "<h1>Hello 2</h1>" > shared2/index.html
+
+docker run -d --rm --name nginx1 \
+  --network tplb \
+  -p 81:80 \
+  -v "$(pwd)/shared1:/usr/share/nginx/html" \
+  nginx
+
+docker run -d --rm --name nginx2 \
+  --network tplb \
+  -p 82:80 \
+  -v "$(pwd)/shared2:/usr/share/nginx/html" \
+  nginx
+
+
+docker run -d --rm --name nginx-lb \
+  --network tplb \
+  -p 83:80 \
+  im-nginx-lb
